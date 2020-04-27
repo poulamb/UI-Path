72350ecd5bd26c6a39e797a14dbc8077219737cc
Poulami <53557205+poulamb@users.noreply.github.com>
2020-04-28 01:29:33 +0530

    Update playground.js

--- ReactPlayground/playground.js
+++ ReactPlayground/playground.js
@@ -6,3 +6,25 @@ ReactDOM.render(
   <Button />, 
   document.getElementById('mountNode'),
 );
+
+/***************************/
+function logRandom(){
+  console.log(Math.random());
+}
+function Button() { 
+  const [clickCount, setClickCount] = useState(0);
+	return <button onClick={()=>setClickCount(clickCount+1)}>{clickCount}</button>;
+}
+
+ReactDOM.render(
+  <Button />, 
+  document.getElementById('mountNode'),
+);
+/*********************************/
+/*********************************/
+/*********************************//*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
