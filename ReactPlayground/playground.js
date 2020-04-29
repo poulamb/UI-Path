bcbceb4b9d26c28d23ef2e0f2922a8a48d5352f0
Poulami <53557205+poulamb@users.noreply.github.com>
2020-04-29 14:43:41 +0530

    Update playground.js

--- ReactPlayground/playground.js
+++ ReactPlayground/playground.js
@@ -34,7 +34,81 @@ ReactDOM.render(
   document.getElementById('mountNode'),
 );
 /*********************************/
-/*********************************//*********************************/
+function Button() {
+	const [counter, setCounter] = useState(0);
+  const handleClick = () => setCounter(counter+1); 
+	return (<button onClick={handleClick}>
+    {counter}
+  </button>
+          );
+}
+
+function Display(){
+  return (
+  <div> ... </div>
+  );
+}
+
+ReactDOM.render(
+<div>
+  <Button />
+  <Display />
+  </div>,
+  document.getElementById('mountNode'),
+);
+/*********************************/
+// same as above without new dom elements, React.Fragment can be replaced by <></>
+function Button() {
+	const [counter, setCounter] = useState(0);
+  const handleClick = () => setCounter(counter+1); 
+	return (<button onClick={handleClick}>
+    {counter}
+  </button>
+          );
+}
+
+function Display(){
+  return (
+  <div> ... </div>
+  );
+}
+
+ReactDOM.render(
+<React.Fragment>
+  <Button />
+  <Display />
+  </React.Fragment>,
+  document.getElementById('mountNode'),
+);
+/*********************************/
+function Button(props) {
+// 	const [counter, setCounter] = useState(25);
+  
+	return (<button onClick={props.handleClick}>
+    +1
+  </button>
+          );
+}
+
+function Display(props){
+  return (
+  <div>{props.msg} </div>
+  );
+}
+function App(){
+  const [counter, setCounter] = useState(25);
+  const handleClick = () => setCounter(counter+1); 
+    return (
+    <div>
+    <Button handleClick = {handleClick}/>
+    <Display msg={counter}/>
+    </div>
+  );
+}
+ReactDOM.render(
+  <App/>,
+  document.getElementById('mountNode'),
+);
 /*********************************/
 /*********************************/
 /*********************************/
