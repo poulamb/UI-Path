91d8e0ad152ce9e0343bf92eccd6de7596430e8d
Poulami <53557205+poulamb@users.noreply.github.com>
2020-04-30 11:19:04 +0530

    Update playground.js

--- ReactPlayground/playground.js
+++ ReactPlayground/playground.js
@@ -184,5 +184,101 @@ ReactDOM.render(
   mountNode,
 );
 /*********************************/
+
+	const testData = [
+			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+	];
+
+
+class Card extends React.Component {
+	render() {
+    const profile = this.props;
+  	return (
+    	<div className="github-profile">
+    	  <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+    	</div>
+    );
+  }
+}
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile=><Card {...profile}/>)}
+  </div>
+  );
+
+class Form extends React.Component {
+  userNameInp = React.createRef();
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.userNameInp.current.value);
+  }
+  render(){
+    return (
+    <form onSubmit={this.handleSubmit}>
+    <input type="text" placeholder = "Github Username" required ref={this.userNameInp}/>
+    <button>Add</button>
+    </form>
+    );
+  }
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      profiles : testData,
+    };
+  }
+  // state = {
+  //   profiles : testData,
+  // }
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form/>
+        <CardList profiles ={ this.state.profiles}/>
+    	</div>
+    );
+  }	
+}
+
+ReactDOM.render(
+	<App title="The GitHub Cards App" />,
+  mountNode,
+);
 /*********************************/
 /*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+/*********************************/
+
