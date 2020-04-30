e7d0c7df57dc66899a9e7dd17855dfb098476614
Poulami <53557205+poulamb@users.noreply.github.com>
2020-04-30 16:38:46 +0530

    Update playground.js

--- ReactPlayground/playground.js
+++ ReactPlayground/playground.js
@@ -337,7 +337,192 @@ ReactDOM.render(
   mountNode,
 );
 /*********************************/
+// STAR MATCH - Starting Template
+
+const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1,9));
+  const btns = 9;
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {utils.range(1,stars).map(starId => <div key={starId} className="star" />)}
+        </div>
+        <div className="right">
+          {utils.range(1,btns).map(btnId => <button key={btnId} className="number">{btnId}</button>)}
+                  </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+ReactDOM.render(<StarMatch />, mountNode);
+
 /*********************************/
+// STAR MATCH - V2
+const PlayNumber = props =>{
+  return (
+  <button  className="number" 
+    style={{backgroundColor : colors[props.status]}}
+    onClick={()=>props.onClick(props.number, props.status)}
+    >{props.number}
+    </button>)
+}
+const StarsDisplay = props =>(
+  // console.log(props.stars);
+    <>
+    {utils.range(1, props.stars).map(starId =>
+  <div key={starId} className="star" />
+      )}
+      </>
+)
+const StarMatch = () => {
+  
+	const [stars, setStars] = useState(utils.random(1, 9));
+//   
+  const [availableNum, setAvailableNum] = useState(utils.range(1, 9));   const [candidateNum, setCandidateNum] = useState([]);
+  const candidatesAreWrong = ()=>{
+   return ( utils.sum(candidateNum)>stars?'wrong':'candidate');
+  };
+ const onNumberClick = (number,status) =>{
+   console.log('num',number);
+    console.log('status',status);
+    if(status=='used'){
+      return ;
+    }
+    const newCandidateNum = 
+          status === 'available'
+          ?candidateNum.concat(number) :candidateNum.filter(
+          cn => cn !== number
+          );
+    if(utils.sum(newCandidateNum) !== stars)
+      {
+        setCandidateNum(newCandidateNum);
+      }
+       else{
+         const newAvailableNum = availableNum.filter(
+         n => !newCandidateNum.includes(n)
+         );
+         setStars(utils.randomSumIn(newAvailableNum,9));
+         setAvailableNum(newAvailableNum);
+         setCandidateNum([]);
+       }
+ } 
+  const numberStatus = (number) =>{
+    if(!availableNum.includes(number)){
+      return 'used';
+    }
+    if(candidateNum.includes(number)){
+      return candidatesAreWrong();
+    } 
+    return 'available';
+  }
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+        <StarsDisplay stars={stars}/>
+       </div>
+       <div className="right">
+        	{utils.range(1, 9).map(number =>
+          <PlayNumber 
+            key={number} 
+            status = {numberStatus(number)}
+            onClick = {onNumberClick}
+            number={number}
+            />
+          )}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+ReactDOM.render(<StarMatch />, mountNode);
 /*********************************/
 /*********************************/
 /*********************************/
